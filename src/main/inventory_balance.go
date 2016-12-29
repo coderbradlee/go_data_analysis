73fb9daae05f60f992c93ea1ceac68f165574977
lzxm160 <lzxm160@sina.com>
2016-12-29 17:31:53 +0800

    first commit

---
+++ src/main/inventory_balance.go
@@ -0,0 +1,61 @@
+ package main
+ 
+import (
+    "fmt"
+    _ "github.com/go-sql-driver/mysql"
+    "net/http"
+)
+/**
+ * 初步获取t_inventory_balance里面的数据
+ */
+type inventory_balance struct {
+    company_id string
+ 	accounting_period_id string
+ 	item_master_id string
+ 	in_stock_balance int32
+ 	uom_id string
+ 	refresh_date string
+}
+func (u *inventory_balance) print(){
+	fmt.Printf("%s||%s||%s||%d||%s||%s\n", u.company_id,u.accounting_period_id,u.item_master_id,u.in_stock_balance,u.uom_id,u.refresh_date)
+}
+func start(w http.ResponseWriter, r *http.Request) {
+    rows, err := db.Query("SELECT company_id,accounting_period_id,item_master_id,in_stock_balance,uom_id,refresh_date FROM t_inventory_balance")
+    defer rows.Close()
+    checkErr(err)
+ 
+    // columns, _ := rows.Columns()
+    // scanArgs := make([]interface{}, len(columns))
+    // values := make([]interface{}, len(columns))
+    // for j := range values {
+    //     scanArgs[j] = &values[j]
+    // }
+
+    var records []*inventory_balance
+	for rows.Next() {
+	    p := new(inventory_balance)
+	    if err := rows.Scan(&p.company_id, &p.accounting_period_id, &p.item_master_id, &p.in_stock_balance, &p.uom_id, &p.refresh_date); err != nil {
+	    	fmt.Println("55")
+	    }
+	    records = append(records, p)
+	}
+
+ 	// print(records)
+    copy(records) 
+    pad_product_category_id(g_insert_data)
+    pad_statistic_time(g_insert_data)
+    pad_createAt(g_insert_data)
+    pad_currency_id(g_insert_data)
+    fmt.Fprintln(w, "finish")
+}
+func print( records []*inventory_balance) {
+	for _,i:=range records{
+		i.print()
+	}
+}
+func checkErr(err error) {
+    if err != nil {
+        fmt.Println(err)
+        panic(err)
+    }
+}
---
+++ src/main/main.go
@@ -0,0 +1,31 @@
+package main
+ 
+import (
+    "database/sql"
+    _"fmt"
+    _ "github.com/go-sql-driver/mysql"
+    "log"
+    "net/http"
+)
+ 
+var db *sql.DB
+ 
+func init() {
+    db, _ = sql.Open("mysql", "renesola:renes0la.xx@tcp(172.18.22.202:3306)/apollo_eu_erp?charset=utf8")
+    db.SetMaxOpenConns(20)
+    db.SetMaxIdleConns(10)
+    db.Ping()
+}
+ 
+func main() {
+    startHttpServer()
+}
+ 
+func startHttpServer() {
+    http.HandleFunc("/pool", start)
+    err := http.ListenAndServe(":9090", nil)
+    if err != nil {
+        log.Fatal("ListenAndServe: ", err)
+    }
+}
+
